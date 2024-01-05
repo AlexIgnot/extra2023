6b5f7783d04328049a7f1c69b208416d79c73c78
AlexIgnot <alex.ignot@hotmail.com>
2024-01-05 11:01:15 -0600

    9 de 10 unit-test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormulaComponent } from './formula/formula.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormulaComponent
   ],
   imports: [
     BrowserModule
